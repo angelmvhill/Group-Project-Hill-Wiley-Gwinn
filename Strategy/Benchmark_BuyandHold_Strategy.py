88747234e6f5ceb2245b7b7dbbd9d1780a935c86
angelmvhill <angel.mv.hill@gmail.com>
2021-12-09 00:20:00 -0500

    update benchmark to take 1 min dataset

--- Strategy/Benchmark_BuyandHold_Strategy.py
+++ Strategy/Benchmark_BuyandHold_Strategy.py
@@ -1,13 +1,26 @@
 import backtrader as bt
-from datetime import datetime
+import datetime
 import os
 
 class BuyandHold(bt.Strategy):
+    """
+    This class serves as the benchmark strategy, or buying and holding the traded asset.
+    """
+
+    def log(self, txt, dt=None):
+        '''Logging function for strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def add_data(cerebro):
+        data=MyFeed()
+        cerebro.add_data(data)
+        return data 
 
     def starting_cash(self):
-        #set starting cash value
+        # set starting cash value
         self.val_start = self.broker.get_cash()
-
+    
     def purchase_shares(self):
         # Buy shares with all available cash
         num_of_shares = int(self.broker.get_cash() / self.data.close)
@@ -18,14 +31,78 @@ class BuyandHold(bt.Strategy):
         self.roi = (self.broker.get_value() / self.val_start) - 1.0
         print('ROI:                     {:.2f}%'.format(100.0 * self.roi))
 
+# class for loading 1 min dataset - wrote by Julian
+import pandas as pd
+from backtrader.feed import DataBase
+from backtrader import date2num
+from backtrader import TimeFrame
 os.chdir(r'C:/Users/angel/Documents/Documents/GitHub/Group-Project-Hill-Wiley-Gwinn/Data')
-data = bt.feeds.YahooFinanceData(dataname="AAPL_1min.csv")
+df = pd.read_csv('AAPL_1min.csv')
+df['datetime'] = pd.to_datetime(df['datetime'])
+class MyFeed(DataBase):
+    def __init__(self):
+        super(MyFeed, self).__init__()
+        self.list = df
+        self.n = 0
 
-cerebro = bt.Cerebro()
-cerebro.adddata(data)
-cerebro.addstrategy(BuyandHold)
-cerebro.broker.setcash(2500)
+        self.fromdate = self.list['datetime'][0]
+        self.todate = self.list['datetime'][len(self.list) - 1]
+        self.timeframe = bt.TimeFrame.Minutes
+        print("from=%s,to=%s" % (self.fromdate, self.todate))
 
-print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-cerebro.run()
-print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+        self.m = {}
+        # print(self.list)
+
+    def start(self):
+        # Nothing to do for this data feed type
+        pass
+
+    def stop(self):
+        # Nothing to do for this data feed type
+        pass
+
+    def _load(self):
+        if self.n >= len(self.list):
+            return False
+
+        r = self.list.iloc[self.n]
+        self.lines.datetime[0] = date2num(r['datetime'])
+
+        self.lines.open[0] = r['open']
+        self.lines.high[0] = r['high']
+        self.lines.low[0] = r['low']
+        self.lines.close[0] = r['close']
+        self.lines.volume[0] = r['volume']
+        self.m[r['datetime']] = r
+
+        self.n = self.n + 1
+        return True
+
+if __name__ == '__main__':
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Add a strategy
+    cerebro.addstrategy(BuyandHold)
+
+    # Create a Data Feed
+
+    data = MyFeed()
+
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
+
+    # Set our desired cash start
+    cerebro.broker.setcash(2500)
+
+    # Set the commission - 0.1% ... divide by 100 to remove the %
+    cerebro.broker.setcommission(commission=0.001)
+
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Run backtest
+    cerebro.run()
+
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
